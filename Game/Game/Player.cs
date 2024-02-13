77b4bb782bb3a9edff406d143375c884da13ed81
Jasper <1056722@hr.nl>
2024-02-13 13:37:53 +0100

    PLAYER ADDED

---
+++ Game/Game/Player.cs
@@ -0,0 +1,28 @@
+class Player
+{   
+    // Properties
+    public int HP { get; set; }
+    public string Location { get; set; }
+    public string Weapon { get; set; }
+    public string Name { get; set; }
+    public int Max_hp = 100;
+
+    
+    // Constructor
+    public Player(int hp, string location, string weapon, string name)
+    {
+        HP = hp;
+        Location = location;
+        Weapon = weapon;
+        Max_hp = 100;
+        Name = name;
+    }
+
+    public void CalculateHP()
+    {
+        
+    }
+
+
+
+}
